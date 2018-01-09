28d59b50441fb16eb6c48a29117e9c743df0b661
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-08 21:03:57 -0600

    Using & understanding className, using it to show Player information

--- app.jsx
+++ app.jsx
@@ -4,8 +4,21 @@ function Application() {
       <div className="header">
         <h1>Scorebord</h1>
       </div>
-    
+
       <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Hugo Mandujano
+          </div>
+
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
