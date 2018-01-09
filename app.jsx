562dfc5f652b1fe0259225401a5c92649449b611
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-08 20:56:48 -0600

    New react application

---
+++ app.jsx
@@ -0,0 +1,14 @@
+function Application() {
+  return (
+    <div className="application">
+      <div className="header">
+        <h1>Scorebord</h1>
+      </div>
+    
+      <div className="players">
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
