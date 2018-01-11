191df5b06a52bc377eaf32b4acd72cc50ba3de43
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-10 22:53:13 -0600

    Communicating events

--- app.jsx
+++ app.jsx
@@ -31,15 +31,16 @@ Header.propTypes = {
 function Counter(props) {
   return (
     <div className="counter">
-      <button className="counter-action decrement"> - </button>
+      <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
       <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
+      <button className="counter-action increment" onClick={function() {props.onChange(1);}}> + </button>
     </div>
   );
 }
 
 Counter.propTypes = {
   score: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired,
 }
 
 
@@ -50,7 +51,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter score={props.score} onChange={props.onScoreChange} />
       </div>
     </div>
   );
@@ -59,6 +60,7 @@ function Player(props) {
 Player.propTypes = {
   name: React.PropTypes.string.isRequired,
   score: React.PropTypes.number.isRequired,
+  onScoreChange: React.PropTypes.func.isRequired,
 };
 
 var Application = React.createClass({
@@ -84,6 +86,10 @@ var Application = React.createClass({
     };
   },
 
+  onScoreChange: function(delta) {
+    console.log('onScoreChange', delta);
+  },
+
   render: function(){
     return (
       <div className="scoreboard">
@@ -91,8 +97,14 @@ var Application = React.createClass({
 
         <div className="players">
           {this.state.players.map(function(player) {
-            return <Player name={player.name} score={player.score} key={player.id} />
-          })}
+            return (
+              <Player
+                  onScoreChange= {this.onScoreChange}
+                  name={player.name}
+                  score={player.score}
+                  key={player.id} />
+            );
+          }.bind(this))}
         </div>
       </div>
     );
