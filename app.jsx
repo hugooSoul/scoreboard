92ff5ee2cab57d896edc28e3d471ab2523e78776
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-10 21:41:37 -0600

    Removing score from Counter & from PropType and added to an State

--- app.jsx
+++ app.jsx
@@ -29,14 +29,19 @@ Header.propTypes = {
 };
 
 var Counter = React.createClass({
-  PropTypes: {
-    score: React.PropTypes.number.isRequired,
+  PropTypes: {},
+
+  getInitialState: function() {
+    return {
+      score: 0,
+    }
   },
+
   render: function(){
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <div className="counter-score"> {this.props.score} </div>
+        <div className="counter-score"> {this.state.score} </div>
         <button className="counter-action increment"> + </button>
       </div>
     );
@@ -50,7 +55,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter />
       </div>
     </div>
   );
